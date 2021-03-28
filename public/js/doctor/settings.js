0e4695f896b58def4adc437ad3807153762d5d77
Rohan Jaiswal <jaiswalrohan8796@gmail.com>
2021-03-28 19:24:47 +0530

    doctor settings page added

---
+++ public/js/doctor/settings.js
@@ -0,0 +1,24 @@
+var inputs = document.querySelectorAll("input");
+const saveButton = document.getElementById("saveButton");
+
+inputs.forEach((input) => {
+    input.disabled = true;
+});
+var isInputDisabled = true;
+
+const enableEdit = () => {
+    if (isInputDisabled) {
+        inputs.forEach((input) => {
+            input.disabled = false;
+        });
+        isInputDisabled = false;
+        saveButton.style.display = "inline";
+    } else {
+        inputs.forEach((input) => {
+            input.disabled = true;
+        });
+        isInputDisabled = true;
+        saveButton.style.display = "none";
+    }
+    console.log("enable input function called");
+};
